3ce4c35a215e5eb11fe8991c2965181aa9b3216e
jeremyyeo <jeremyyeo@gmail.com>
2023-12-07 13:31:59 +1300

    update

--- macros/gan.sql
+++
@@ -1,5 +0,0 @@
-
-{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
-    {%- set ts = modules.datetime.datetime.now().timestamp() | string -%}
-    {{ node.name ~ "_" ~ ts.replace(".", "_") | trim }}
-{%- endmacro %}
