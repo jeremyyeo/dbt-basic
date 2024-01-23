00ff03612e122a30f493f68975b517037a5fbcc0
jeremyyeo <jeremyyeo@gmail.com>
2024-01-23 22:53:05 +1300

    update

--- models/foo_050.sql
+++ models/foo_050.sql
@@ -1,15 +1,7 @@
 
 {{ config(materialized = 'table') }}
 
-{% set results = run_query("select explode(sequence(DATE'1970-01-01', DATE'2020-01-01', INTERVAL 1 DAY)) as dates") %}
-
-{% if execute %}
-    {% set results_list = results.columns[0].values() %}
-{% else %}
-    {% set results_list = [] %}
-{% endif %}
-
 select 
-{% for r in results_list -%}
-    1 as col_{{ r.strftime('%Y%m%d') }} {%- if not loop.last -%} , {%- endif -%}
+{% for n in range(100_000) -%}
+    1 as col_{{ n }} {%- if not loop.last -%} , {%- endif -%}
 {%- endfor %}
