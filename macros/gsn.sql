deff3286cd57310f6973706f75fe02de0b2de43a
jeremyyeo <jeremyyeo@gmail.com>
2024-06-11 12:40:16 +1200

    update

--- macros/gsn.sql
+++
@@ -1,7 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if target.name == 'ci' -%}
-        {{ custom_schema_name }}_ci_{{ target.schema }}
-    {%- else -%}
-        {{ custom_schema_name }}
-    {%- endif -%}
-{%- endmacro %}
---
+++ models/snek.py
@@ -0,0 +1,8 @@
+import pandas as pd
+def model(dbt, session):
+    dbt.config(
+        submission_method="all_purpose_cluster",
+        cluster_id="0426-154059-dox4kjfg"
+    )
+    a = b
+    return pd.DataFrame({"id": [1]})
