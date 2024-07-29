a87fae800d0ba62f46f5482adaf802238930c861
jeremyyeo <jeremyyeo@gmail.com>
2024-07-30 10:33:15 +1200

    update

--- models/mart/bar.sql
+++ models/mart/bar.sql
@@ -1,6 +1,6 @@
 
 {%- set q -%}
-select s_nationkey from raw_tpch.tpch_sf1000.supplier group by 1
+select s_nationkey from raw_tpch.tpch_sf1000.supplier
 {%- endset -%}
 
 {%- set results = run_query(q) -%}
