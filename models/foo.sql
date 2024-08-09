3975e31001cf16114488fca29aa076b016aa4e5e
jeremyyeo <jeremyyeo@gmail.com>
2024-08-09 19:29:53 +1200

    update

--- models/foo.sql
+++ models/foo.sql
@@ -1,18 +1,3 @@
-{% set V_BATCH_ID = 123 %}
+{{ config(materialized='table') }}
 
-WITH 
-
-FINAL_SQL AS (
-SELECT 
-    MD5(OBJECT_CONSTRUCT ('COL1',123::STRING
-                        , 'COL2',CURRENT_DATE::STRING
-                        , 'COL3','MODELN'::STRING
-                        )::STRING) AS FUNNEL_KEY, -- SINCE WE DON'T HAVE PROPER NK, WE ARE GOING WITH OID
-    'alice' AS USER_NAME
-  FROM DUAL
-)
-
-SELECT FUNNEL_KEY,
-       USER_NAME,
-       {{V_BATCH_ID}}::NUMBER(38,0) SOME_KEY
-  FROM FINAL_SQL
+select 1 id
