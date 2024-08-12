3b5a837c904f1856ff040cd42047e93d24ae2cb2
jeremyyeo <jeremyyeo@gmail.com>
2024-08-12 13:35:33 +1200

    update

--- models/metrics/metricflow_time_spine.sql
+++ models/metrics/metricflow_time_spine.sql
@@ -1,18 +1,24 @@
-{{ config(materialized = 'table') }}
+{{
+    config(
+        materialized = 'table',
+    )
+}}
 
 with days as (
-   {{
-       dbt_utils.date_spine(
-           'day',
-           "to_date('01/01/2000','mm/dd/yyyy')",
-           "to_date('01/01/2027','mm/dd/yyyy')"
-       )
-   }}
+
+    {{
+        dbt.date_spine(
+            'day',
+            "to_date('01/01/2000','mm/dd/yyyy')",
+            "to_date('01/01/2027','mm/dd/yyyy')"
+        )
+    }}
+
 ),
 
 final as (
     select cast(date_day as date) as date_day
-      from days
+    from days
 )
 
 select * from final
