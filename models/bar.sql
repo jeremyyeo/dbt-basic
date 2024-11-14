acf4bbcba155ab87979fb48e6656c2fa5784153c
jeremyyeo <jeremyyeo@gmail.com>
2024-11-15 12:21:05 +1300

    update

--- models/bar.sql
+++ models/bar.sql
@@ -1,1 +1,22 @@
-select 1 id
+{{
+    config(
+        materialized='incremental',
+        unique_key = ['user_id'],
+        incremental_strategy='merge',
+    )
+}}
+
+with activity_daily as (
+    select * from {{ ref('foo') }}
+    where
+        event_date >= current_date - interval '5 years'
+
+    {% if is_incremental() %}
+
+        and event_date > current_date - interval '1 day'
+
+    {% endif %}
+)
+
+select *
+from activity_daily
