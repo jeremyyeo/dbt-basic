36d050f4b104ccc29772e919cc4a920d500545db
jeremyyeo <jeremyyeo@gmail.com>
2025-07-28 15:12:16 +1200

    update

--- macros/m.sql
+++
@@ -1,6 +0,0 @@
-{% macro m() %}
-    {% if execute %}
-        {% set r = run_query('select current_timestamp as c') %}
-        {% do print('The database time is ' ~ r.columns[0].values()[0]) %}
-    {% endif %}
-{% endmacro %}
