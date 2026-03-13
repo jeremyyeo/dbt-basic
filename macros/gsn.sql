4bf174f0a5696b2e3b136e25c47aca7ae050865e
jeremyyeo <jeremyyeo@gmail.com>
2026-03-13 12:48:54 -0700

    update

--- macros/gsn.sql
+++
@@ -1,7 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {% if target.name == 'prod' %}
-        {{ custom_schema_name }}
-    {% else %}
-        {{ target.schema }}
-    {% endif %}
-{%- endmacro %}
