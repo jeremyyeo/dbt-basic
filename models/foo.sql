31476dfacfc7547ef266ca0dca984a37096c94ca
jeremyyeo <jeremyyeo@gmail.com>
2026-03-25 13:28:09 -0700

    update

--- models/foo.sql
+++ models/foo.sql
@@ -5,3 +5,9 @@
 }}
 
 select 1 id, 'alice' as first_name
+
+{% if is_incremental() %}
+  -- this filter will only be applied on an incremental run
+  where id > (select max(id) from {{ this }})
+
+{% endif %}
