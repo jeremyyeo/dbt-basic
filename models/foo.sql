c4fd63a22a789dd41f605a38c301bd563dd7f20c
jeremyyeo <jeremyyeo@gmail.com>
2026-03-27 10:24:12 -0700

    update

--- models/foo.sql
+++ models/foo.sql
@@ -1,13 +1,3 @@
-{{ 
-    config(
-        materialized='incremental' 
-    ) 
-}}
-
-select 1 id, 'alice' as first_name
-
-{% if is_incremental() %}
-  -- this filter will only be applied on an incremental run
-  where id > (select max(id) from {{ this }})
-
-{% endif %}
+select 1 as c
+union all
+select 1 as c
